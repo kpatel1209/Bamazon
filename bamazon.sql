3a21a3cd2cefada2a48515dac9e3dea01a6ebc54
Kevin Patel <kpatel1209@gmail.com>
2019-02-13 18:25:42 -0500

    have not pushed in a while

--- bamazon.sql
+++ bamazon.sql
@@ -1,20 +1,20 @@
-CREATE DATABASE bamazon;
+CREATE DATABASE bamazon_db;
 
-USE bamazon;
+USE bamazon_db;
 
 CREATE TABLE products(
-    ID INT NOT NULL AUTO_INCREMENT,
-    PRODUCT VARCHAR (100) NULL,
-    DEPARTMENT VARCHAR (100) NULL,
-    PRICE DECIMAL (10, 2) NOT NULL,
-    QTY INT default 0 NOT NULL,
-    SALES DECIMAL (10, 2),
-    PRIMARY KEY (id)
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name TEXT NOT NULL,
+    department_name TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    product_sales DECIMAL(10, 2),
+    PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product, department, price, stock) VALUES
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES
 ("TV Remote", "Electronics", 29.99, 100),
-("Pillow", "Home", "12.95", "200"),
+("Pillow", "Home", 12.95, 200),
 ("Phone", "Electronics", 499.97, 100),
 ("Knives", "Kitchen", 50.75, 250),
 ("Candles", "Home", 10.99, 500),
@@ -23,4 +23,3 @@ INSERT INTO products (product, department, price, stock) VALUES
 ("Race Car", "Toys", 51.75, 100),
 ("Silverware", "Kitchen", 25.69, 100),
 ("Alarm Clock", "Electronics", 19.99, 40);
-
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,96 +1,160 @@
-const inquirer = require('inquirer');
 const mysql = require('mysql');
-const Table = require('cli-table'); // allows you to render unicode-aided tables on the command line from your node.js scripts
-const chalk = require('chalk'); // adds styling options
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const Table = require('cli-table');
 
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
-    database: "bamazon"
+    database: "bamazon_db"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Successful Connection!");
-    displayAllProducts();
+    // Added \xa0 for spacing so "Welcome to Bamazon!" is centered over the product table.
+    console.log(chalk.whiteBright("\n\t\xa0\xa0\xa0\xa0Welcome to Bamazon!\n"));
+    showAllProducts();
 });
 
-let userChoice = {};
+var userChoice = {};
 
-// Function that displays all items for sale, but does not show the quantity available
-function displayAllProducts() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        let productTable = new Table({
-            head: ["ID", "PRODUCT", "DEPARTMENT", "PRICE"],
+// Function that emptys the userChoice array so a new product can be placed inside of an empty array.
+var emptyUserChoice = function() {
+    userChoice = {};
+}
+
+// Function that shows all items for sale, but does not show the quantity available
+var showAllProducts = function() {
+    connection.query("SELECT * FROM products", (err, res) => {
+        let productListing = new Table({
+            head: [chalk.blueBright.bold("ID"), chalk.blueBright.bold("PRODUCT NAME"), chalk.blueBright.bold("DEPARTMENT"), chalk.blueBright.bold("PRICE")],
         });
-        for (var i = 0; i < res.length; i++) {
-            productTable.push([chalk.blue(res[i].ID), res[i].PRODUCT, res[i].DEPARTMENT, chalk.green(`$${res[i].PRICE}`)]);
+        for (let i = 0; i < res.length; i++) {
+            productListing.push([chalk.red.bold(res[i].item_id), res[i].product_name, res[i].department_name, chalk.greenBright(`$${res[i].price}`)]);
         }
-        console.log(`${productTable.toString()}`);
-        connection.end();
+        console.log(`${productListing.toString()}`);
+        
+        productID();
     });
 };
 
 // Function that asks the user to enter the product ID for the item they want to purchase
-function purchaseByID() {
+var productID = function() {
     inquirer.prompt({
-        name: "ID",
+        name: "productID",
         type: "input",
-        message: "Please enter the ID of the product you would like to purchase today.",
-        // Check if the ID entered by user is a number between 1-10
+        message: "Hello! Please refer to the table above and enter the product ID you would like to purchase today.",
+        // Check if the product ID entered by the user is a number between 1-10
         validate: function(value) {
-            if(value > 0 && value <= 10 && !isNaN(value)) {
+            if (!isNaN(value) && (value > 0 && value <= 10)) {
                 return true;
             } else {
-                console.log(chalk.red("An ID number between 1-10 is required!"));
+                console.log(chalk.yellow("You must enter a product ID between 1-10 to move forward with your order."));
                 return false;
             }
         }
-    }).then(function(answer){
-        connection.query("SELECT ID, PRODUCT, DEPARTMENT, PRICE, QTY, SALES FROM products WHERE ?", {ID: answer.ID}, function(err, res){
+    }).then(function(answer) {
+        connection.query("SELECT item_id, product_name, department_name, price, stock_quantity, product_sales FROM products WHERE ?", { item_id: answer.productID }, function(err, res){
             // Check with the user if this is the correct product they want to purchase.
-            itemCheck(res[0].PRODUCT, res);
+            productCheck(res[0].product_name, res);
         });
     });
 };
 
-function itemCheck(product, object) {
+// Function to check if the product chosen is correct
+var productCheck = function(product, object) {
     inquirer.prompt({
-        name: "itemCheck",
+        name: "productCheck",
         type: "confirm",
-        message: `Is the ` + chalk.red(`'${product}'`) + ` correct?`
+        message: `Please confirm if ` + chalk.yellow.bold(`'${product}'`) + ` is the correct product you want to purchase?`
     }).then(function(answer) {
-        if (answer.itemCheck) {
+        if (answer.productCheck) {
             userChoice = {
-                ID: object[0].ID,
-                PRODUCT: object[0].PRODUCT,
-                DEPARTMENT: object[0].DEPARTMENT,
-                PRICE: object[0].PRICE,
-                QTY: object[0].QTY,
-                SALES: object[0].SALES
+                item_id: object[0].item_id,
+                product_name: object[0].product_name,
+                department_name: object[0].department_name,
+                price: object[0].price,
+                stock_quantity: object[0].stock_quantity,
+                product_sales: object[0].product_sales
             };
-            // Ask how many of the items the user would like to purchase.
-            quantityNeeded(userChoice.ID);
+            // Ask user the quantiy needed to purchase
+            quantityNeeded(userChoice.item_id);
         } else {
-            purchaseByID();
+            productID();
         }
     });
 };
 
 // Function for the quantity needed to purchase by the user
-function quantityNeeded(userChoice) {
+var quantityNeeded = function() {
     inquirer.prompt({
         name: "quantityNeeded",
-        type: "input",
-        message: "How many do you need?",
+        type: 'input',
+        message: "How many would you like to purchase today?",
         validate: function(value) {
-            if (value > 0 && !ifNaN(value)) {
+            if (!isNaN(value) && value > 0) {
                 return true;
             } else {
-                console.log()
+                console.log(chalk.yellow("Please enter a number between 1-10"));
+                return false;
             }
         }
-    })
+    }).then(function(answer) {
+        connection.query("SELECT stock_quantity FROM products WHERE ?", { item_id: userChoice.item_id }, function(err, res){
+            // If there is not enough quantity to fulfill the order.
+             if (res[0].stock_quantity < answer.quantityNeeded) {
+                console.log(chalk.yellow.bold("\n\tSorry for any inconvenience, but we currently do not have that quantity amount in stock.\n"));
+                inquirer.prompt({
+                    name: "continue",
+                    type: "confirm",
+                    message: "Do you want to order a lesser quantity and try again?"
+                }).then(function(answer) {
+                    if (answer.continue) {
+                        quantityNeeded(userChoice.item_id);
+                    } else {
+                        console.log(chalk.blue.bold('\n\tThank you, please visit us again soon!\n'));
+                        connection.end();
+                    }
+                });
+            // If there is enough quantity available then the order will move forward
+            } else {
+                userChoice.quantityNeeded = answer.quantityNeeded;
+                console.log(chalk.red.bold("\n\tWe are processing your order..."));
+
+                // MYSQL database needs to be updated with new quantity available and sales
+                connection.query('UPDATE products SET ? WHERE ?', [
+                    {
+                        stock_quantity: userChoice.stock_quantity - answer.quantityNeeded,
+                        product_sales: userChoice.product_sales + (userChoice.price * answer.quantityNeeded)
+                    },
+                    {
+                        item_id: userChoice.item_id
+                    }
+                ], function(err, res) {
+                    console.log(chalk.greenBright.bold(`\n\tThe total amount for your order is $${(userChoice.price * userChoice.quantityNeeded)}.\n`));
+                    // ask if user would like to make another purchase
+                    newOrder();
+                });
+            }
+        });
+    });
 }
+
+// function to ask if user would like to make another purchase
+var newOrder = function() {
+    inquirer.prompt({
+        name: "newOrder",
+        type: "confirm",
+        message: "Would you like to place another order?"
+    }).then(function(answer){ 
+        if (answer.newOrder) {
+            emptyUserChoice();
+            productID();
+        } else {
+            console.log(chalk.yellow.bold("\n\tThanks for visiting Bamazon!  See you soon!"));
+            connection.end();
+        }
+    });
+};
